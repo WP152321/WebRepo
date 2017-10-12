47179dd26c3951f366390ed8364b32f7ae31cd6e
ldcle <ldcle@DESKTOP-EAUBS8C>
2017-10-12 17:03:25 +0900

    5

--- WebClass/src/org/dimigo/servlet/BlogLoginServlet.java
+++ WebClass/src/org/dimigo/servlet/BlogLoginServlet.java
@@ -32,7 +32,7 @@ public class BlogLoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher rd =request.getRequestDispatcher("myblog/blog.jsp");
+		RequestDispatcher rd =request.getRequestDispatcher("myblog/login.jsp");
 		rd.forward(request, response);
 	}
 
